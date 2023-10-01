7425335f369c8cf2117ec7a3282815215aa85544
mateusz.golonka <mateusz.golonka@HK-11>
2023-10-01 16:55:37 +0200

    Add project files.

---
+++ Calculator/Controllers/HomeController.cs
@@ -0,0 +1,59 @@
+﻿using Calculator.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace Calculator.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewBag.Imie = "Mateusz";
+            ViewBag.godzina = DateTime.Now.Hour;
+            ViewBag.powitanie = ViewBag.godzina < 17 ? "Dzień dobry" : "Dobry wieczór";
+
+            Dane[] osoby =
+            {
+                new Dane {Name = "Mateusz", Surname = "Golonka"},
+                new Dane {Name = "Mateusz", Surname = "Nowak"},
+                new Dane {Name = "Mateusz", Surname = "Kowalski"},
+
+            };
+            return View(osoby);
+        }
+        public IActionResult Urodziny(Urodziny urodziny)
+        {
+            ViewBag.powitanie = $"Witaj {urodziny.Imie} masz {DateTime.Now.Year - urodziny.Rok} lat";
+            return View();
+        }
+        public IActionResult Kalkulator(Urodziny kalkulator)
+        {
+            ViewBag.powitanie = "Witaj w prostym kalkulatorze";
+
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Calculator/Models/kalkulator.cs
@@ -0,0 +1,8 @@
+﻿namespace Calculator.Models
+{
+    public class kalkulator
+    {
+        public int Liczba1 { get; set; }
+        public int Liczba2 { get; set; }
+    }
+}
